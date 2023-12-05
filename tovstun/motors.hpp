e69fade39812b6af6c5527359df52339ff33ecc3
Artem Trokhymchuk <trkhmchk@gmail.com>
2023-12-05 23:43:12 +0200

    initial state processing

--- tovstun/motors.hpp
+++ tovstun/motors.hpp
@@ -15,29 +15,24 @@ struct Flag {
   static constexpr int RAISED = 120;
   static constexpr int LOWERED = 50;
 
-  Flag() {
-    flag.attach(flag_servo_pin);
-  }
+  Flag() { flag.attach(flag_servo_pin); }
 
-  void raise() {
-    flag.write(RAISED);
-  }
+  void raise() { flag.write(RAISED); }
 
-  void lower() {
-    flag.write(LOWERED);
-  }
+  void lower() { flag.write(LOWERED); }
 };
 
 extern Flag flag;
 
-
 struct Motors {
 
-  void attach() {
+  void attach()
+  {
     l_motor.attach(left_motor_pin);
     r_motor.attach(right_motor_pin);
   }
-  void prepare_forward() {
+  void prepare_forward()
+  {
     if (l_prev_speed >= 90 && r_prev_speed >= 90)
       return;
     unsigned long prevMillis = millis();
@@ -51,7 +46,8 @@ struct Motors {
   // я навіть не буду намагатися писати коментарі англійською
   // в цьому коді правди нема, а баги є
   // воно таке страшне тільки тому, щоб перемикати назад не за 12с, а за 6(а то і менше)
-  void prepare_backward() {
+  void prepare_backward()
+  {
     if (l_prev_speed < 90 && r_prev_speed < 90)
       return;
     unsigned long prevMillis = millis();
@@ -98,21 +94,24 @@ struct Motors {
     }
   }
 
-  void move_forward(int speed) {
+  void move_forward(int speed)
+  {
     prepare_forward();
     speed += 90;
     l_prev_speed = speed;
     r_prev_speed = speed;
     write_for(speed, 20);
   }
-  void move_backward(int speed) {
+  void move_backward(int speed)
+  {
     prepare_backward();
     speed = 90 - speed;
     l_prev_speed = speed;
     r_prev_speed = speed;
     write_for(speed, 20);
   }
-  prepare_left_forward_right_backward() {
+  void prepare_left_forward_right_backward()
+  {
     if (l_prev_speed >= 90 && r_prev_speed < 90)
       return;
     unsigned long prevMillis = millis();
@@ -143,7 +142,8 @@ struct Motors {
         r_motor.write(87);
     }
   }
-    prepare_left_backward_right_forward() {
+  void prepare_left_backward_right_forward()
+  {
     if (r_prev_speed >= 90 && l_prev_speed < 90)
       return;
     unsigned long prevMillis = millis();
@@ -175,18 +175,18 @@ struct Motors {
     }
   }
 
-  void write_for(int speed, unsigned long miliseconds) {
+  void write_for(int speed, unsigned long miliseconds)
+  {
     unsigned long prevMillis = millis();
     while (millis() - prevMillis <= miliseconds) {
       l_motor.write(speed);
       r_motor.write(speed);
     }
   }
-
-  int l_prev_speed = 90,
-      r_prev_speed = 90;
-  Servo l_motor,
-    r_motor;
+  // ASSUME: speed for motors is equal
+  int get_unite_speed() { return l_prev_speed - 90; }
+  int l_prev_speed = 90, r_prev_speed = 90;
+  Servo l_motor, r_motor;
 };
 
 extern Motors motors;
--- tovstun/tovstun.ino
+++ tovstun/tovstun.ino
@@ -1,6 +1,6 @@
 
-#include "sensors.hpp"
 #include "motors.hpp"
+#include "sensors.hpp"
 #include "state.hpp"
 //#include "display.hpp"
 //#include "debug.hpp"
@@ -8,11 +8,12 @@
 State state = State::Default;
 unsigned long prev_time = 0;
 unsigned long state_duration = 0; // For how long current state lasts
-
 State next_state = State::Default;
-StateData next_state_data{};
+State prev_state = State::Default;
+StateData state_data{};
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   motors.attach();
   Serial.println("Hello world!");
@@ -20,32 +21,35 @@ void setup() {
   prev_time = millis();
 }
 
-void apply_movement() {
+void apply_movement()
+{
   // TODO: Apply movement depending on current and next state
 }
 
-void tick_state_transition(const SensorsData& sensors) {
+void tick_state_transition(const SensorsData &sensors)
+{
   // TODO: Smth along the lines of the following
   /*
   if (state == State::AccelToSpeed) {
     if (state_duration > 500)
       next_state = State::HoldSpeed;
     if (sensors.is_forward_left_obstacle) {
-      
+
     }
   } else if (state == State::HoldSpeed) {
     next_state = State::AccelToSpeed;
-    next_state_data.speed = 69;
+    state_data.speed = 69;
   }
   */
 }
 
-void tick_state_machine() {
+void tick_state_machine()
+{
   const unsigned long curr_time = millis();
   const unsigned long dt = curr_time - prev_time;
   prev_time = curr_time;
   state_duration += dt;
-  
+
   const SensorsData sensors = SensorsData::read();
 
   tick_state_transition(sensors);
@@ -60,33 +64,79 @@ void tick_state_machine() {
 
 // int speed = 0;
 
-void loop() {
-  Serial.println("Hello world!");
+void loop()
+{
+  prev_state = state;
+  //  tick_state_machine();
+  //  debug_display_print_illumination();
 
-  tick_state_machine();  
+  // if there is something in the front: accel + positive speed
+  if ((forward_left_ultrasonic.read() <= 20 && forward_left_ultrasonic.read() != 0) ||
+      (forward_right_ultrasonic.read() <= 20 && forward_right_ultrasonic.read() != 0)) {
+    state = State::AccelToSpeed;
+    state_data.speed = 10;
+    // if there is nothing at front or sideways, accel + negative speed
+  }
+  else if (forward_left_ultrasonic.read() > 20 && forward_right_ultrasonic.read() > 20) {
+    state = State::AccelToSpeed;
+    state_data.speed = -10;
+  }
 
-  // debug_display_print_illumination();
-  // if(forward_left_ultrasonic.read() <= 20 || forward_right_ultrasonic.read() <= 20) {
-  //   if(forward_left_illumination_sensor.collides() || forward_left_illumination_sensor.collides()) {
-  //     speed = 0;
-  //     move_forward(0);
-  //   } else{
-  //     if(speed < 30)
-  //       speed += 5;
-  //     move_forward(speed);
-  //   }
-  // } else if(forward_left_ultrasonic.read() > 20 || forward_right_ultrasonic.read() > 20) {
-  //   if(backward_left_illumination_sensor.collides()) {
-  //     speed = 0;
-  //     move_backward(0);
-  //   } else{
-  //     if(speed < 30)
-  //       speed += 5;
-  //     move_backward(speed);
-  //   }
-  // }
- // Serial.println(forward_left_ultrasonic.read());
-  
-  // Serial.println(forward_right_ultrasonic.read());
-  // delay(100);
+  // THAT MUST BE LAST IFS: stop if we see white line
+  if (state_data.speed > 0 && forward_right_illumination_sensor.collides() ||
+      forward_left_illumination_sensor.collides()) {
+    state = State::Stop;
+  }
+  if (state_data.speed < 0 && backward_left_illumination_sensor.collides()) {
+    state = State::Stop;
+  }
+  // State: accelerating
+  if (state == State::AccelToSpeed) {
+    // if prev state was also accelerating
+    if (prev_state == State::AccelToSpeed) {
+      // if previously we were acclerating to the same direction
+      if (motors.get_unite_speed() >= 0 && state_data.speed >= 0 ||
+          motors.get_unite_speed() < 0 && state_data.speed < 0) {
+        // just continue to accelerate
+        // if we need to accelerate more
+        if (abs(motors.get_unite_speed()) < abs(state_data.speed)) {
+          if (state_data.speed > 0)
+            motors.move_forward(motors.get_unite_speed() + 3);
+          else if (state_data.speed < 0)
+            motors.move_backward(abs(motors.get_unite_speed()) + 3);
+          Serial.println("increasing the speed");
+        }
+        Serial.println("speed is ok");
+        // ok, we are accelereting, but motors are in different state
+      }
+      else {
+        Serial.println("speed is not unite");
+        Serial.print("Motor: ");
+        Serial.println(motors.get_unite_speed());
+        if (state_data.speed > 0) {
+          motors.prepare_forward();
+          motors.move_forward(3);
+          Serial.println("Preparing moving forward");
+        }
+        else {
+          motors.prepare_forward();
+          motors.move_backward(3);
+          Serial.println("Preparing moving backward");
+        }
+      }
+    }
+    else {
+      if (state_data.speed > 0) {
+        motors.prepare_forward();
+        motors.move_forward(3);
+        Serial.println("Preparing moving forward");
+      }
+      else {
+        motors.prepare_forward();
+        motors.move_backward(3);
+        Serial.println("Preparing moving backward");
+      }
+    }
+  }
+  delay(1000);
 }
