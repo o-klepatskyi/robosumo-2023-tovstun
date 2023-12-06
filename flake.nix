afb94f4e1997d89ea7c815697ea294cef08e26a3
Artem Trokhymchuk <trkhmchk@gmail.com>
2023-12-06 15:44:37 +0200

    some nix stuff + gitignore

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "cpp env";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              clang-tools
+              pkgsCross.avr.buildPackages.gcc
+              pkgsCross.avr.buildPackages.binutils
+              arduino-language-server
+              arduino
+              arduino-core
+              screen
+            ];
+            shellHook = ''
+            '';
+          };
+        }
+      );
+}
